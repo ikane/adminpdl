91d4eb624210206bfdae1b05af3f5e483b33781d
Ibrahima KANE <ibrahima_kane@carrefour.com>
2020-06-14 12:29:41 +0200

    ajout appel api gouv

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Recherche from './components/Recherche';
 import './App.css';
+import { Message } from 'semantic-ui-react';
 /*
 function App() {
   return (
@@ -24,11 +25,40 @@ function App() {
 }
 */
 class App extends Component {
+
+  state = {
+    data: [],
+    error: ''
+  }
+
+  onSearch = async (departement, type) => {
+    //console.log(departement, type);
+    if (departement && type) {
+      try {
+        let response = await fetch(`https://etablissements-publics.api.gouv.fr/v3/departements/${departement}/${type}`);
+        let data = await response.json();
+        this.setState({
+          data: data.features,
+          error: ''
+        });
+      } catch (error) {
+        this.setState({ error: 'Erreur lors de la recherche' })
+      }
+    } else {
+      this.setState({ error: 'Merci de choisir un département et un établissement' })
+    }
+  }
+
+  onEmpty = () => {
+    this.setState({ data: [], error: '' });
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <h1>Annuaire des administrations en Pays de la Loire</h1>
-        <Recherche />
+        <Recherche onSearch={this.onSearch} onEmpty={this.onEmpty} />
+        {this.state.error ? <Message warning>{this.state.error}</Message> : undefined}
       </div>
     );
   }
--- src/components/Recherche.js
+++ src/components/Recherche.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Button, Select } from "semantic-ui-react";
+import './recherche.css'
 
 class Recherche extends Component {
 
@@ -8,11 +9,11 @@ class Recherche extends Component {
         type: ''
     }
 
-    departementChanged = (e, data) => {
+    onDepartementChange = (e, data) => {
         this.setState({ departement: data.value });
     }
 
-    typeChanged = (e, data) => {
+    onTypeChange = (e, data) => {
         this.setState({ type: data.value });
     }
 
@@ -21,7 +22,9 @@ class Recherche extends Component {
             { key: "44", value: "44", text: "Loire Atlantique" },
             { key: "49", value: "49", text: "Maine et Loire" },
             { key: "53", value: "53", text: "Mayenne" },
+            { key: "75", value: "75", text: "Paris" },
             { key: "72", value: "72", text: "Sarthe" },
+            { key: "94", value: "94", text: "Val de Marne" },
             { key: "85", value: "85", text: "Vendée" }
         ];
         const optionsAdministration = [
@@ -31,10 +34,10 @@ class Recherche extends Component {
         ];
         return (
             <div className="recherche">
-                <Select placeholder="Choisissez un département" onChange={this.departementChanged} options={optionsDpt} value={this.state.departement} />
-                <Select placeholder="Choisissez une administration" onChange={this.typeChanged} options={optionsAdministration} value={this.state.type} />
-                <Button primary>Lancer la recherche</Button>
-                <Button secondary>Vider la recherche</Button>
+                <Select placeholder="Choisissez un département" onChange={this.onDepartementChange} options={optionsDpt} value={this.state.departement} />
+                <Select placeholder="Choisissez une administration" onChange={this.onTypeChange} options={optionsAdministration} value={this.state.type} />
+                <Button primary onClick={() => this.props.onSearch(this.state.departement, this.state.type)}>Lancer la recherche</Button>
+                <Button secondary onClick={this.props.onEmpty}>Vider la recherche</Button>
             </div>
         );
     }
